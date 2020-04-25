dcc58539ace2c66a103cfcf1b261f980468e9221
Joe Spicuzza <spicuzza21@gmail.com>
2020-04-25 09:30:01 -0400

    stretch solution isEqual

--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -4,23 +4,29 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(multiply()).toThrow('Please enter two numbers');
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('2')).toThrow('Please enter a number');
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(typeof(multiply(2,2))).toBe('number');
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(2,2)).toBe(4);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(10,5)).toBe(50);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
     // test some random values
+
+    let num1=Math.floor((Math.random() * 100) + 1)
+    let num2=Math.floor((Math.random() * 100) + 1)
+
+    expect(multiply(num1,num2)).toBe(num1*num2);
+
   });
 });
