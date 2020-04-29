9f8201e491bbc1cc819447cc5cc851e659620f2a
Joe Spicuzza <spicuzza21@gmail.com>
2020-04-29 18:51:06 -0400

    stretch 5

--- questions/03_anagrams/anagrams.js
+++ questions/03_anagrams/anagrams.js
@@ -3,6 +3,21 @@
 
 const checkAnagrams = (str1, str2) => {
   //YOUR CODE
+  for (let i=0; i<str1.length; i++){
+    let currentLetter =str1[i]
+    if (!str2.includes(currentLetter)){
+      return false
+    }
+  }
+
+  for (let i=0; i<str2.length; i++){
+    let currentLetter =str2[i]
+    if (!str1.includes(currentLetter)){
+      return false
+    }
+  }
+
+  return true
 };
 
 module.exports = { checkAnagrams };
