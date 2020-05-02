6c388d8043297cb3160760a706eef15986560033
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-02 09:19:14 -0400

    stretch 6

--- questions/04_palindrome/palindrome.js
+++ questions/04_palindrome/palindrome.js
@@ -1,8 +1,18 @@
 /* Given a string, determine if it is a palindrome */
 /* A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam, or racecar */
 
-const validPalindrome = () => {
-  //YOUR CODE
+const validPalindrome = (str) => {
+  let secondIdx=str.length-1;
+
+  for (var i=0; i<str.length; i++){
+      if (str[i]===str[secondIdx]){
+        secondIdx--
+      } else {
+        return false
+      }
+  }
+
+return true
 };
 
 module.exports = { validPalindrome };
