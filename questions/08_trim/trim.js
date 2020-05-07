ae8287d52c81d253091632b1dd1a12c4ad18244f
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-07 19:59:10 -0400

    5.7

--- questions/08_trim/trim.js
+++ questions/08_trim/trim.js
@@ -2,6 +2,14 @@
 
 const trim = obj => {
   // YOUR CODE
+  let newObj={}
+
+    for (let key in obj){
+      if (obj[key]!=undefined || obj[key]!= null){
+        newObj[key]= obj[key]
+      }
+    }
+  return newObj
 };
 
 module.exports = { trim };
