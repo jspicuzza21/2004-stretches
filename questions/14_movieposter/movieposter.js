5fa7fa52860c27092cf1de8a7a6da008fa9ba970
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-13 18:51:30 -0400

    5.13

--- questions/14_movieposter/movieposter.js
+++ questions/14_movieposter/movieposter.js
@@ -7,8 +7,37 @@
 // * Play *
 // ********
 
-const movieposter = () => {
+const movieposter = (arr) => {
   //write code here
+  let newArr=[]
+  let top =''
+  let bottom=''
+  let longestWord=''
+
+  for (let i=0; i<arr.length; i++){
+    if (arr[i].length>longestWord.length){
+      longestWord=arr[i]
+    }
+  }
+  for(let n=0; n<longestWord.length+3; n++){
+    top+='*'
+  }
+  newArr.push(top)
+  for (let i=0; i<arr.length; i++){
+    let currentStr=`* ${arr[i]}`
+    for (let j=arr[i].length; j<longestWord.length+1; j++){
+      currentStr+=' '
+    }
+    currentStr+='*'
+    newArr.push(currentStr)
+  }
+  
+  for(let n=0; n<longestWord.length+3; n++){
+    bottom+='*'
+  }
+  
+  newArr.push(bottom)
+  return newArr.join('\n')
 };
 
 module.exports = { movieposter };
