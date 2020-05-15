6bb4e16a6f4e7a57b75488d35af017de9db0a881
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-15 17:26:39 -0400

    5.15

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,22 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+    let arr=[]
+    let numberToAdd=1;
+    let previousEntry=''
+    while (arr.length!==n){
+        if (arr.length!==0){
+          previousEntry=arr[arr.length-1]
+        }
+        let innerArr=[]
+        for (let i=0; i<previousEntry.length+1; i++){
+            innerArr.push(numberToAdd)
+            numberToAdd++
+        }
+        arr.push(innerArr)
+    }
+    return arr.join('\n').replace(/,/g, '')
+}
 
 module.exports = { floydsTriangle };
