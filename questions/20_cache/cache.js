6924859c63cd3c65fe1ffa6d0f41cec80f02b714
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-19 20:15:39 -0400

    5.19

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,26 @@
 // see test specs
-
-function cache() {
+function cache(func) {
   // YOUR CODE
+
+  if (typeof(func)!=='function'){
+    throw ('Input must be a function.');
+  }
+  let cache={};
+
+  return function(...arg){
+    let previousArgs=Object.keys(cache);
+    if (previousArgs.includes(arg+'')){
+      return cache[arg]
+    } else {
+      const newArr = arg.map(element=>func(element*1))
+      const result = newArr.reduce(function(sum, currentNum){
+        return sum+=currentNum
+        },0)
+      cache[arg]=result;
+      return result;
+    }
+  }
+
 }
 
 module.exports = { cache };
