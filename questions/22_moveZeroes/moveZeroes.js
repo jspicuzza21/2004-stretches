1a8775c9295ead43bbb1b1f588be7ae9933b881a
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-21 20:32:02 -0400

    5.21

--- questions/22_moveZeroes/moveZeroes.js
+++ questions/22_moveZeroes/moveZeroes.js
@@ -1,5 +1,16 @@
 const moveZeroes = (arr) => {
-  //YOUR CODE HERE
+  let zeroCount=0;
+  for (let i=0; i<arr.length; i++){
+    if (arr[i]===0){
+      arr.splice(i, 1)
+      i--
+      zeroCount++
+    }
+  }
+  for (let j=0; j<zeroCount; j++){
+    arr.unshift(0)
+  }
+  return arr
 };
 
 module.exports = { moveZeroes };
