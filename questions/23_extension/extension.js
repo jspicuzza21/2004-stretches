335d5446e0fffc3cf9ddcf73b20c9e1fdc0c214d
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-22 17:26:00 -0400

    5.22

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,22 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (func) => {
+    if (typeof(func)!== 'function'){
+        throw('Not a function')
+    }
+    
+    return function newFunc (num) {
+        if (typeof (num)!=='number'){
+            throw('Not a number')
+        }
 
+        return function newFunc2(num2) {
+            if (typeof (num2) !== 'number'){
+                throw('Not a number')
+            }
+            return func(num, num2)
+        }
+    }
+
+};
 module.exports = { extensions };
