cbf4ed7ca9b78a27bd9b08b23fc5fe28db494bc5
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-26 20:31:39 -0400

    5.26

--- questions/26_rps/rps.js
+++ questions/26_rps/rps.js
@@ -8,6 +8,50 @@ Rock defeats Scissors
 
 class RPS {
   //YOUR CODE HERE
+  constructor(...players){
+    this.scoreBoard={}
+    players.forEach(player=>this.scoreBoard[player]=0)
+  }
+
+  play(...moves){
+    const playerNames=Object.keys(this.scoreBoard);
+    for (let i=0; i<moves.length; i++){
+      let currentPlayerMove=moves[i];
+      for (let j=0; j<moves.length; j++){
+        
+        if (currentPlayerMove==='rock'){
+          if(moves[j]==='scissors'){
+            const player=playerNames[i];
+            this.scoreBoard[player]+=1
+          }
+        }
+        if (currentPlayerMove==='paper'){
+          if(moves[j]==='rock'){
+            const player=playerNames[i];
+            this.scoreBoard[player]+=1
+          }
+        }
+        if (currentPlayerMove==='scissors'){
+          if (moves[j]==='rock'){
+            const player=playerNames[i];
+            this.scoreBoard[player]+=1
+          }
+        }
+      }
+    } 
+  }
+
+  winner(){
+    let win=''
+    let highScore=0;
+    for (let key in this.scoreBoard){
+      if (this.scoreBoard[key]>highScore){
+       highScore=this.scoreBoard[key];
+       win=key;
+      }
+    }
+    return `The victor is ${win} with ${highScore} points.`
+  }
 }
 
 module.exports = { RPS };
