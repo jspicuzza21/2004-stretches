0c08b4f85cb3cede3e6b4d28718e2b9a5a13e07f
Joe Spicuzza <spicuzza21@gmail.com>
2020-05-28 16:32:02 -0400

    5.28

--- questions/27_cartesian/cartesian.js
+++ questions/27_cartesian/cartesian.js
@@ -1,5 +1,30 @@
-const cartesian = () => {
+const cartesian = (arr) => {
   //YOUR CODE GOES HERE
+
+  const directions ={
+    n:0,
+    s:0,
+    e:0,
+    w:0
+  }
+
+  arr.forEach(direction => {
+    directions[direction]=directions[direction]+1
+  });
+
+  const NS = Math.min(directions.n, directions.s);
+  const EW= Math.min(directions.e, directions.w);
+  directions.n-=NS;
+  directions.s-=NS;
+  directions.e-=EW;
+  directions.w-=EW;
+
+  if (directions.n===directions.s && directions.e===directions.w){
+    return "These directions don't go anywhere!"
+  }
+
+  return directions;
+
 };
 
 module.exports = { cartesian };
