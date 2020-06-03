f00e795b52c6ff91cd4fa87d4d4558835be6482c
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-03 19:00:06 -0400

    6.3

--- questions/33_directory/directory.js
+++ questions/33_directory/directory.js
@@ -10,8 +10,23 @@
 // };
 // directory(phonebookData)=>{ 'a/b/c': 12 }
 
-const directory = () => {
+const directory = (obj) => {
   //code in here
+  let str=''
+  let returnObj={}
+
+  for (let key in obj){
+    let currentElement = obj[key];
+    str+=key;
+    if (typeof(currentElement==='object')){
+      return str+= `/${directory(currentElement)}`;
+    } else {
+      returnObj.str=currentElement;
+      break;
+    }
+  }
+  return returnObj;
 };
+
 
 module.exports = { directory };
