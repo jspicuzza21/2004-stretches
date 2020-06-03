3c9cebb1d748795cea6ee7f3aa27105726188fab
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-03 11:04:18 -0400

    6.3

--- questions/32_diff2/diff2.js
+++ questions/32_diff2/diff2.js
@@ -2,6 +2,24 @@
 // the returning array of pairs should be sorted in ascending order of values.
 //diffTwo([1, 2, 3, 4])=>[[1, 3], [2, 4]]
 
-const diffTwo = () => {};
+const diffTwo = (arr) => {
+    if (!Array.isArray(arr)){
+        throw new Error()
+    }
+
+    const newArr=[];
+
+    arr.sort((a,b)=>a-b)
+
+    arr.forEach(currentNum=>{
+        for (let i=0; i<arr.length; i++){
+            if (arr[i]-currentNum===2){
+                newArr.push([currentNum,arr[i]])
+            }
+        }
+    })
+
+    return newArr
+};
 
 module.exports = { diffTwo };
