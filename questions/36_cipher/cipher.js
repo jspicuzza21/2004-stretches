6644ae114eabe5c04a44fc345aec56959956e614
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-08 17:50:34 -0400

    6.8

--- questions/36_cipher/cipher.js
+++ questions/36_cipher/cipher.js
@@ -1,5 +1,26 @@
-const encrypt = () => {
+const encrypt = (arg) => {
   //YOUR CODE HERE
+  let newStr=[];
+  let str= arg.split(' ');
+
+  for (let i=0; i<str.length; i++){
+    let currentWord=str[0];
+    let currentEncryptedWord=[]
+    for (let j=0; j<currentWord.length; j++){
+      if(j===0){
+        currentEncryptedWord.push(currentWord.charCodeAt(currentWord[j]))
+      } else if (j===1){
+        currentEncryptedWord.push(currentWord[currentWord.length-1]);
+
+      } else if (j < currentWord.length-1){
+        currentEncryptedWord.push(currentWord[j])
+      } else {
+        currentEncryptedWord.push(currentWord[1])
+      }
+    }
+    newStr.push(currentEncryptedWord.join(''))
+  }
+  return newStr.join('')
 };
 
 module.exports = { encrypt };
