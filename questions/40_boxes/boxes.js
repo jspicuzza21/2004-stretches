7ae7e6bf689e9a8c4281f6b340eb81716591c35b
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-13 09:23:38 -0400

    6.13

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -7,9 +7,25 @@ class Box {
     }
 
     pack(color, capacity){
-        this.contents.push(new Box(color, capacity))
+        if (this.contents.length > capacity){
+            throw new Error()
+        } else {
+            this.contents.push(new Box(color, capacity))
+        }
     }
 
+    unpack(){
+        let colors=[];
+        if (!this.contents.length){
+            colors.push(this.color)
+        } else {
+            colors.push(this.color)
+            for (let i =0; i<this.contents.length; i++){
+                colors.push(...this.contents[i].unpack())
+            }
+        }
+        return colors
+    }
 }
 
 module.exports = { Box };
