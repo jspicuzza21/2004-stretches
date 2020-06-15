eeacb2a35ca05756eeb9d93935705e636ee0230a
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-15 18:51:23 -0400

    6.15

--- questions/44_iterator/iterator.js
+++ questions/44_iterator/iterator.js
@@ -3,6 +3,26 @@
 
 class Iterator {
   //write code here
+  constructor(arr){
+    this.input=arr.slice();
+    this.idx=0;
+  }
+
+  next(){
+    let returnVal={}
+    if (!Array.isArray(this.input)){
+      throw new Error ()
+    }
+
+    if (this.idx===this.input.length){
+      return { done: true, value: undefined }
+    } else {
+      let returnVal= { done: false, value: this.input[this.idx]}
+      this.idx=this.idx+1;
+      return returnVal
+    }
+  }
+
 }
 
 module.exports = { Iterator };
