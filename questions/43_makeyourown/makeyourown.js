b80c1e21e7c55661449ffa32d51369f38b74a6e3
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-14 21:43:07 -0400

    6.14

--- questions/43_makeyourown/makeyourown.js
+++ questions/43_makeyourown/makeyourown.js
@@ -4,8 +4,22 @@
 // Jest documentation: https://github.com/sapegin/jest-cheat-sheet
 // submit the stretch with solution code and test specs passing
 
-const makeyourown = () => {
-  console.log('make your own stretch!');
+const makeyourown = (string) => {
+    let newStr='';
+
+    if (typeof string!=='string'){
+      return 'Please enter a string'
+    }
+    
+    for (let i=0; i<string.length; i++){
+      if (string[i] ===string[i].toUpperCase() && string[i]!==' '){
+        newStr +=' ';
+        newStr += string[i]
+      } else {
+        newStr += string[i]
+      }
+    }
+  return newStr;
 };
 
 module.exports = { makeyourown };
--- questions/43_makeyourown/makeyourown.spec.js
+++ questions/43_makeyourown/makeyourown.spec.js
@@ -1,7 +1,13 @@
 const { makeyourown } = require('./makeyourown');
 
 describe('makeyourown functionality', () => {
-  it('your first test spec', () => {
-    expect(makeyourown()).toEqual();
+  it('Takes only a string and throws ans error if not a string', () => {
+    expect(makeyourown([])).toEqual('Please enter a string');
+  });
+  it('takes a camel case string and turns it into two words', () => {
+    expect(makeyourown('camelCase')).toEqual('camel Case');
+  });
+  it('cam take in multiple words', () => {
+    expect(makeyourown('camelCase camelCase camelCase')).toEqual('camel Case camel Case camel Case');
   });
 });
