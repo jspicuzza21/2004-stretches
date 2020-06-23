8822fb56cce2891add565dba43aa1d394347bb9c
Joe Spicuzza <spicuzza21@gmail.com>
2020-06-23 16:19:31 -0400

    6.23

--- questions/49_calc/calc.js
+++ questions/49_calc/calc.js
@@ -2,6 +2,32 @@
 
 class chainCalc {
   //write code here
+  constructor(num){
+    if (typeof num !== 'number') throw new Error();
+    this.total=num
+    this.result
+  }
+
+  add(addNum){
+    this.result=this.total+addNum;
+    this.total=this.total+addNum;
+    return this
+  }
+
+  sub(subNum){
+    this.result=this.total-subNum;
+    this.total=this.total-subNum;
+    return this
+  }
+
+  mul(multNum){
+    this.result=this.total*multNum;
+    this.total=this.total*multNum;
+    return this
+  }
+
+  
 }
+
 
 module.exports = { chainCalc };
