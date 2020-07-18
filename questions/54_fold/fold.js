7a8b1e452a7bd2a09bad2cc5afdb0d9c1cd62c01
Joe Spicuzza <spicuzza21@gmail.com>
2020-07-18 09:22:32 -0400

    7.18

--- questions/54_fold/fold.js
+++ questions/54_fold/fold.js
@@ -1,5 +1,12 @@
-const fold = () => {
-  //YOUR CODE HERE
+const fold = (arr, num) => {
+  let newArr=[]
+  if (arr.length===1||num===0){
+   return arr
+  } else {
+    newArr.push(fold(arr.slice(1,arr.length-1), num-1))
+    newArr.push(arr[0] + arr[arr.length-1])
+  }
+  return newArr
 };
 
 module.exports = fold;
